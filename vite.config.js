762c5084ccfd9f2d6fabeb9718525bd09c645321
sakura <knikiforov017@gmail.com>
2025-05-19 16:33:30 +0300

    install gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/portfolio/',  // имя репозитория с ведущим и завершающим слэшем
   plugins: [
     react(),
     tailwindcss(),
